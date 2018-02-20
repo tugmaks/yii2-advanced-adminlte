14ef4b9f607c838722eb5b159b987e4f73c98949
Maksim Tyugaev <maksim.tyugaev@alfaforex.com>
2018-02-20 15:01:32 +0300

    init

---
+++ backend/views/layouts/_main-footer.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\base\View;
+
+/**
+ * @var View $this View object itself
+ */
+?>
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+</footer>
---
+++ backend/views/layouts/_wrapper.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\base\View;
+
+/**
+ * @var View   $this    View object itself
+ * @var string $content Something that should be rendered
+ */
+?>
+
+<div class="wrapper">
+    <?= $this->render('_main-header'); ?>
+    <?= $this->render('_main-sidebar'); ?>
+    <?= $this->render('_content', ['content' => $content]); ?>
+    <?= $this->render('_main-footer') ?>
+    <?= $this->render('_control-sidebar') ?>
+</div>
